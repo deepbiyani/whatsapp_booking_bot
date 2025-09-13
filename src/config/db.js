9b3abf1625b504d64f0b9b07f7e92a1bd5db0427
Deepanshu <deepbiyani@gmail.com>
2025-09-13 22:36:53 +0530

    qw

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 const logger = require("../utils/logger");
+require("dotenv").config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/eventbot");
+        console.log(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL);
         logger.info("✅ MongoDB connected");
     } catch (err) {
         logger.error("❌ MongoDB connection error: " + err.message);
