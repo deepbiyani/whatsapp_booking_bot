276d9d7e9060279a8d2c41d327a45f12ebb24974
user <deepanshu.biyani@nesoftmail.com>
2025-09-21 10:29:00 +0530

    Implemented qr code api

--- src/scheduler.js
+++ src/scheduler.js
@@ -20,7 +20,7 @@ function startScheduler(client) {
                     const media = MessageMedia.fromFilePath(outputPath);
 
                     const number = `91${booking.phone}@c.us`
-                    const message = `🎉 Thank You for Your Booking! 🎉\nHello ${booking.name},\n\nYour booking for Divine Events has been successfully received ✅.\n📌 Pass Type: ${booking.type}\n👥 Members: ${booking.members}\n💰 Amount Paid: ₹${booking.totalPaid}\n\nYour entry pass will be valid on Event Day. Kindly show this confirmation at the gate for smooth entry.\n\n✨ We look forward to celebrating with you at Devine Events!\n\nFor any queries, reply to this message or contact our support 📞 7058746046.\n\n– Team Divine Events 🌟`;
+                    const message = `🎉 Thank You for Your Booking! 🎉\nHello ${booking.name},\n\nYour booking for Divine Events has been successfully received ✅.\n📌 Pass Type: ${booking.type}\n👥 Members: ${booking.members}\n💰 Amount Paid: ₹${booking.totalPaid}\n\nYour entry pass will be valid on Event Day. Kindly show this confirmation at the gate for smooth entry.\n\n✨ We look forward to celebrating with you at Divine Events!\n\nFor any queries, reply to this message or contact our support 📞 7058746046.\n\n– Team Divine Events 🌟`;
                     await client.sendMessage(number, media, { sendMediaAsDocument: true, caption : message });
 
                     booking.passSent = true;
